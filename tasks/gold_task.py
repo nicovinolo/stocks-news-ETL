95b2ba511e17e3b1da1bcd1e61a7be70de7d2641
nicovinolo <102926956+nicovinolo@users.noreply.github.com>
2024-10-24 19:00:21 -0300

    Update gold_task.py

--- tasks/gold_task.py
+++ tasks/gold_task.py
@@ -1,4 +1,3 @@
-from airflow.exceptions import AirflowException
 from datetime import timedelta
 
 from utils.engine_creation import create_redshift_engine_from_env
@@ -17,9 +16,6 @@ def gold_task_f(**context):
         execution_date_str = execution_date.strftime('%Y-%m-%d')
 
         populate_company_insights(connection, execution_date_str)
-
-    except AirflowException as e:
-        raise e
 
 
 if __name__ == "__main__":
