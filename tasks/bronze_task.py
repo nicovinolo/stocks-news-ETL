95c6e7611ed4a9ccaa5c34e05fdcf8ca683b96b1
nicovinolo <nicolasvinolo98@gmail.com>
2024-10-24 19:10:07 -0300

    adapt to linting

--- tasks/bronze_task.py
+++ tasks/bronze_task.py
@@ -5,12 +5,9 @@ from datetime import timedelta
 
 def bronze_task_f(**context):
 
-    try:
-        execution_date = context['execution_date'] - timedelta(days=1)
-
-        execution_date_str = execution_date.strftime('%Y-%m-%d')
-
-        parquet_creation(companies, api_key, execution_date_str)
+    execution_date = context['execution_date'] - timedelta(days=1)
+    execution_date_str = execution_date.strftime('%Y-%m-%d')
+    parquet_creation(companies, api_key, execution_date_str)
 
 
 if __name__ == "__main__":
--- tasks/silver_task_01.py
+++ tasks/silver_task_01.py
@@ -9,21 +9,13 @@ from silver.db_data_insertion import insert_news_data_into_redshift
 
 def silver_task_01_f(**context):
 
-    try:
-        connection = create_redshift_engine_from_env()
-
-        # Create schema in db if it doesn't exist
-        create_redshift_tables(connection)
-
-        execution_date = context['execution_date'] - timedelta(days=1)
-
-        execution_date_str = execution_date.strftime('%Y-%m-%d')
-
-        insert_company_data_into_redshift(connection, execution_date_str)
-        insert_news_data_into_redshift(connection, execution_date_str)
-
-    except AirflowException as e:
-        raise e
+    connection = create_redshift_engine_from_env()
+    # Create schema in db if it doesn't exist
+    create_redshift_tables(connection)
+    execution_date = context['execution_date'] - timedelta(days=1)
+    execution_date_str = execution_date.strftime('%Y-%m-%d')
+    insert_company_data_into_redshift(connection, execution_date_str)
+    insert_news_data_into_redshift(connection, execution_date_str)
 
 
 if __name__ == "__main__":
